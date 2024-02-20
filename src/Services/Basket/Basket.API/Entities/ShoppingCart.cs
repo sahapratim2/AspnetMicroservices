c0daaa1b5243a1e7c36eb0a3db590e64e7bf617c
Partha Pratim Saha <sahapratim2@gmail.com>
2024-02-20 17:43:35 -0600

    Section 3.37

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,31 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+
+        public ShoppingCart()
+        {
+
+        }
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+
+                Items.ForEach(item => totalprice += item.Price * item.Quantity);
+
+                return totalprice;
+            }
+        }
+
+    }
+}
